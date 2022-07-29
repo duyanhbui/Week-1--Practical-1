8d68e8acfd4d5e79067e875f18b840e446ae8b56
duyanhbui <duyanh.bui@student.adelaide.edu.au>
2022-07-29 10:10:23 +0930

    hope this is the last one

--- function-3-4.cpp
+++ function-3-4.cpp
@@ -4,22 +4,22 @@ void print_pass_fail(char grade) {
     bool pass = true;
     switch (grade){
         case 'A':
-            std :: cout << "Pass \n";
+            std :: cout << "Pass\n";
             break;
         case 'B':
-            std :: cout << "Pass \n";
+            std :: cout << "Pass\n";
             break;
         case 'C':
-            std :: cout << "Pass \n";
+            std :: cout << "Pass\n";
             break;
         case 'D':
-            std :: cout << "Fail \n";
+            std :: cout << "Fail\n";
             break;
         case 'E':
-            std :: cout << "Fail \n";
+            std :: cout << "Fail\n";
             break;    
         default:
-            std :: cout << "Nothing \n";
+            std :: cout << "Nothing\n";
             break;
         
     }
