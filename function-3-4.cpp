764f5f8dd9a35316485cdd3ae79077240727401d
duyanhbui <duyanh.bui@student.adelaide.edu.au>
2022-07-29 10:01:10 +0930

    fifth commit

--- function-3-4.cpp
+++ function-3-4.cpp
@@ -4,22 +4,22 @@ void print_pass_fail(char grade) {
     bool pass = true;
     switch (grade){
         case 'A':
-            std :: cout << "Pass";
+            std :: cout << "Pass \n";
             break;
         case 'B':
-            std :: cout << "Pass";
+            std :: cout << "Pass \n";
             break;
         case 'C':
-            std :: cout << "Pass";
+            std :: cout << "Pass \n";
             break;
         case 'D':
-            std :: cout << "Fail";
+            std :: cout << "Fail \n";
             break;
         case 'E':
-            std :: cout << "Fail";
+            std :: cout << "Fail \n";
             break;    
         default:
-            std :: cout << "Fail";
+            std :: cout << "Fail \n";
             break;
         
     }
